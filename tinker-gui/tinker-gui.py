0d4d0281d9c5380852bed773b2aa9d43354656d2
samckp <sam.mca.liet@gmail.com>
2020-05-24 08:12:00 +0530

    GUI demo app

--- tinker-gui/tinker-gui.py
+++ tinker-gui/tinker-gui.py
@@ -1,9 +1,22 @@
 from tkinter import *
+from tkinter.ttk import *
+
 window = Tk()
-window.title("Welcome to Tinker GUI App")
+window.title("Welcome to LikeGeeks app")
 window.geometry('350x200')
-lbl = Label(window, text="GUI Testing ")
+lbl = Label(window, text="Hello")
 lbl.grid(column=0, row=0)
-btn = Button(window, text="Click Me")
-btn.grid(column=1, row=0)
+txt = Entry(window,width=10)
+txt.grid(column=1, row=0)
+def clicked():
+    res = "Welcome to " + txt.get()
+    lbl.configure(text= res)
+btn = Button(window, text="Click Me", command=clicked)
+btn.grid(column=2, row=0)
+
+combo = Combobox(window)
+combo['values']= (1, 2, 3, 4, 5, "Text")
+combo.current(1) #set the selected item
+combo.grid(column=0, row=0)
+
 window.mainloop()
