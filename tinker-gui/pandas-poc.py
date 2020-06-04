e4f56ed81fa33cb22bda9f80c479c7fa5fbe9364
samckp <sam.mca.liet@gmail.com>
2020-06-04 09:04:36 +0530

    pandas poc

---
+++ tinker-gui/pandas-poc.py
@@ -0,0 +1,8 @@
+dict = {"country": ["Brazil", "Russia", "India", "USA", "South Africa"],
+       "capital": ["Brasilia", "Moscow", "New Dehli", "Washington", "Pretoria"],
+       "area": [8.516, 17.10, 3.286, 9.597, 1.221],
+       "population": [200.4, 143.5, 1252, 1357, 52.98] }
+
+import pandas as pd
+brics = pd.DataFrame(dict)
+print(brics)
